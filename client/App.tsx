9ae23118a58ce9b1c3b20cb2c1ec6f2938041165
jaxon <jaxon@gowithdev.com>
2025-07-16 11:06:25 +0700

    fix

--- client/App.tsx
+++ client/App.tsx
@@ -21,7 +21,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/ocean" element={<OceanGame />} />
-          {/* <Route path="/ocean" element={<OceanGame />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
